77664e9028b7802fd4589cd8becdbac7b1376a4c
Nilsharr <nilsharrr@gmail.com>
2022-06-20 20:47:34 +0200

    Add computing entropy and average codeword length

--- huff.py
+++ huff.py
@@ -1,5 +1,6 @@
 from collections import Counter
 import turtle
+from scipy.stats import entropy
 
 
 class Node:
@@ -11,15 +12,16 @@ class Node:
         self.huff = ''
 
 
-def printNodes(node: Node, val=''):
+def getNodes(node: Node, val='', resultDict={}) -> dict:
     newVal = val + str(node.huff)
-
     if node.left:
-        printNodes(node.left, newVal)
+        getNodes(node.left, newVal, resultDict)
     if node.right:
-        printNodes(node.right, newVal)
+        getNodes(node.right, newVal, resultDict)
     if(not node.left and not node.right):
-        print(f"{node.symbol}: {newVal}")
+        # print(node.symbol, newVal)
+        resultDict.update({node.symbol: newVal})
+    return resultDict
 
 
 def createTree(word: str) -> Node:
@@ -72,11 +74,39 @@ def drawTree(root: Node):
     turtle.mainloop()
 
 
+def textEntropy(text: str) -> float:
+    # we only consider UTF8 characters to compute the text entropy
+    pk = list(Counter(text).values())
+    if sum(pk) == 0:
+        return None
+    else:
+        return entropy(pk, base=2)
+
+
+def averageCodeWordLength(word: str, markCodeDict: dict) -> float:
+    # markCodeDict contains symbol and it's huffman code e.g. a: '00'
+    # markProbDict contains symbol and it's probability e.g. a: 0.125
+    markProbDict = {key: value / len(word)
+                    for key, value in dict(Counter(word)).items()}
+    if ' ' in markProbDict:
+        markProbDict["[space]"] = markProbDict.pop(' ')
+    return sum([markProbDict[key] * len(markCodeDict[key]) for key in markProbDict])
+
+
 if __name__ == '__main__':
     word = 'A' * 10 + 'E' * 15 + 'I' * 12 + 'S' * 3 + 'T' * 4 + 'P' * 13 + ' '
-    word = 'Teleinformatyka'
+    word = 'Betty Botter bought some butter; But she said "this butter''s bitter!" If i put it in my batter It would make my batter bitter. So she bought a bit of butter Better than the bitter butter. Put it in the bitter tabber Make the bitter batter better. So t''was better Betty Botter Bought a bit of better butter.'
+
     print(dict(Counter(word)))
     node = createTree(word)
-    printNodes(node)
+    markCodeDict = getNodes(node)
+    # sort by code length
+    [print(f'{symbol}: {code}')
+     for symbol, code in dict(sorted(markCodeDict.items(), key=lambda x: len(x[1]))).items()]
+
+    print(f'Entropy = {textEntropy(word)}')
+    print(
+        f'Average code word length = {averageCodeWordLength(word, markCodeDict)}')
+    print(
+        f'Efficency = {round(textEntropy(word) / averageCodeWordLength(word, markCodeDict) * 100, 2)}%')
     drawTree(node)
-    # turtle.TurtleScreen._RUNNING = True
